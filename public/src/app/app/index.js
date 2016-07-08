63f925fc1ffa3e35b535381a006a0fd3c7694f1b
Stas Kogut <staskgt@nixsolutions.com>
2016-07-08 10:40:04 +0300

    implement card-holder component

--- public/src/app/app/index.js
+++ public/src/app/app/index.js
@@ -1,14 +1,10 @@
 import { Component } from 'angular2/core';
-import { CardHolderComponent } from '../card-holder';
+import { CardsComponent } from '../cards/cards.component';
 
 @Component({
   selector: 'my-app',
-  directives: [CardHolderComponent],
-  template: '<card-holder></card-holder>'
+  directives: [CardsComponent],
+  template: '<all-cards></all-cards>'
 })
 
-export class AppComponent {
-  constructor() {
-    this.name = 'world';
-  }
-}
+export class AppComponent { }
--- public/src/app/card-holder/index.js
+++
@@ -1,7 +0,0 @@
-import { Component } from 'angular2/core';
-
-@Component({
-  selector: 'card-holder',
-  template: require('./card-holder.component.html')
-})
-export class CardHolderComponent { }
