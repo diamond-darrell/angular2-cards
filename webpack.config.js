b40aa69f63f2f97c2256b59ae3579d79856a50d7
Stas Kogut <staskgt@gmail.com>
2016-07-10 17:56:53 +0300

    create server.js for run app

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
         path.join(__dirname, 'src', 'index.js')
     ],
     output: {
-        path: path.join(__dirname, 'dist'),
+        path: path.join(__dirname, 'public', 'dist'),
         filename: 'vendor.js',
         publicPath: '/public/dist'
     },
