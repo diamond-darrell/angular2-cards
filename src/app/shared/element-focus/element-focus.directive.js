c79cfd4988845f7d14864619c9ed4d7775fb7377
Stas Kogut <staskgt@nixsolutions.com>
2016-07-11 15:43:32 +0300

    implement element focus directive

---
+++ src/app/shared/element-focus/element-focus.directive.js
@@ -0,0 +1,20 @@
+import { Directive, Input, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[focus]'
+})
+export class FocusDirective {
+  @Input() focus = false;
+
+  static get parameters() {
+    return [[ElementRef]];
+  }
+
+  constructor(element) {
+    this.element = element;
+  }
+
+  ngOnChanges() {
+    this.element.nativeElement.focus();
+  }
+}
