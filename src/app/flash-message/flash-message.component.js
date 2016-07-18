4eb27736052c1f30f570a358af37d4815f3ee575
Stas Kogut <staskgt@nixsolutions.com>
2016-07-18 16:36:20 +0300

    implement stack of flash messages

--- src/app/flash-message/flash-message.component.js
+++ src/app/flash-message/flash-message.component.js
@@ -5,15 +5,19 @@ import { FlashMessageService } from 'service/flash-message.service';
   selector: 'flash-message',
   providers: [FlashMessageService],
   template: `
-    <div *ngFor="let message of messages" class="flash-message fade in alert" [ngClass]="message.alertType">
-      <span>{{message.message}}</span>
+    <div class="col-md-4 col-md-offset-8 flash-messages">
+      <div *ngFor="let message of messages" class="fade in alert" [ngClass]="message.alertType">
+        <span>{{message.message}}</span>
+      </div>
     </div>
   `,
   styles: [`
-    .flash-message {
-      position: fixed;
-      top: 10px;
-      right: 10px;
+    @media (min-width: 992px) {
+      .flash-messages {
+        position: absolute;
+        top: 10px;
+        right: -5px;
+      }
     }
   `]
 })
@@ -21,12 +25,17 @@ export class FlashMessageComponent {
   @Input() data = {};
 
   messages = [];
+  maxStackCount = 5;
 
   ngOnChanges() {
     const { type, message } = this.data;
     if (!!type) {
       const alertType = `alert-${type}`;
 
+      if (this.messages.length > this.maxStackCount) {
+        this.messages.shift();
+      }
+      
       this.messages.push({alertType, message});
 
       setTimeout(() => {
