135c8552f01ba669bd50bc389746540b2bf2177e
Stas Kogut <staskgt@nixsolutions.com>
2016-07-20 16:38:21 +0300

    add collection.util test

---
+++ src/app/utils/collection/collection.util.spec.js
@@ -0,0 +1,37 @@
+import collection from 'utils/collection/collection.util';
+
+describe('Collection util test', () => {
+  it('Should has functions', () => {
+    expect(typeof(collection.addItem)).toBe('function');
+    expect(typeof(collection.updateItem)).toBe('function');
+    expect(typeof(collection.removeItem)).toBe('function');
+  });
+
+  it('Function addItem should add item into array', () => {
+    let arr = [];
+    arr = collection.addItem(arr, 'Test');
+
+    expect(arr.length).toBe(1);
+    expect(arr).toContain('Test');
+  });
+
+  it('Function removeItem should remove item from array', () => {
+    let arr = ['Test'];
+    arr = collection.removeItem(arr, 'Test');
+
+    expect(arr.length).toBe(0);
+    expect(arr).not.toContain('Test');
+  });
+
+  it('Function updateItem should update item in array', () => {
+    const item = { id: 1, text: 'Test' };
+    let arr = [item];
+
+    item.text = 'Test1';
+
+    arr = collection.updateItem(arr, item);
+
+    expect(arr.length).toBe(1);
+    expect(arr).toContain(item);
+  });
+});
