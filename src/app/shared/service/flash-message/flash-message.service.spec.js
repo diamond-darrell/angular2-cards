8652f19601af14dc059ad805aad50b8540e71d94
Stas Kogut <staskgt@nixsolutions.com>
2016-07-22 16:18:05 +0300

    add flash-message.service test

---
+++ src/app/shared/service/flash-message/flash-message.service.spec.js
@@ -0,0 +1,53 @@
+import { Subject } from 'rxjs/Subject';
+import { Observable } from 'rxjs/Observable';
+
+import { FlashMessageService } from 'service/flash-message/flash-message.service';
+
+import {
+  beforeEach,
+  beforeEachProviders,
+  describe,
+  expect,
+  it,
+  inject,
+} from '@angular/core/testing';
+
+describe('Flash-message service test', () => {
+  let service: FlashMessageService;
+
+  beforeEachProviders(() => [FlashMessageService]);
+
+  beforeEach(inject([FlashMessageService], (fmService: FlashMessageService) => {
+    service = fmService;
+  }));
+
+  it('should has properties', () => {
+    expect(service.showFleshMessageSource).toBeDefined();
+    expect(service.showFleshMessage$).toBeDefined();
+
+    expect(service.showFleshMessageSource instanceof Subject).toBeTrue();
+    expect(service.showFleshMessage$ instanceof Observable).toBeTrue();
+  });
+
+
+  it('showMessage should emit event', () => {
+    service.showFleshMessage$.subscribe(
+      ({ type, message }) => {
+        expect(type).toBe('success');
+        expect(message).toBe('test');
+      }
+    );
+
+    service.showMessage('success', 'test');
+  });
+
+  it('showMessage should receive allowed types', () => {
+    const allowedTypes = ['success', 'error', 'info', 'warning'];
+
+    allowedTypes.forEach(type => {
+      expect(() => service.showMessage(type, 'message')).not.toThrow();
+    });
+
+    expect(() => service.showMessage('wrong', 'message')).toThrow();
+  });
+});
