81e19e2eb55737ae52ec9a110f9535f374048cd8
Stas Kogut <staskgt@nixsolutions.com>
2016-07-25 18:32:14 +0300

    add add-row-btn test

---
+++ src/app/add-row-btn/add-row-btn.component.spec.js
@@ -0,0 +1,43 @@
+import { AddRowBtnComponent } from 'app/add-row-btn/add-row-btn.component';
+
+import {
+  beforeEach,
+  beforeEachProviders,
+  describe,
+  expect,
+  it,
+  inject,
+  TestComponentBuilder,
+} from '@angular/core/testing';
+
+describe('BoardComponent test', () => {
+  let tcb;
+
+  beforeEachProviders(() => [
+    TestComponentBuilder,
+    AddRowBtnComponent,
+  ]);
+
+  beforeEach(inject([TestComponentBuilder], _tcb => {
+    tcb = _tcb;
+  }));
+
+  it('emit onAddRow event', (done) => {
+    tcb.createAsync(AddRowBtnComponent).then(fixture => {
+      const component = fixture.componentInstance;
+      const element = fixture.nativeElement;
+      const test = {
+        expectCalled() { },
+      };
+
+      spyOn(test, 'expectCalled');
+      component.onAddRow.subscribe(() => test.expectCalled());
+
+      element.querySelector('.add-row').click();
+
+      expect(test.expectCalled).toHaveBeenCalled();
+
+      done();
+    }).catch(e => done.fail(e));
+  });
+});
