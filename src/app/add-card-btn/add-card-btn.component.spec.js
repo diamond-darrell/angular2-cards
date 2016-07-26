211fc3e2ce6190b75cf1eb6dba58a5c18ec71605
Stas Kogut <staskgt@nixsolutions.com>
2016-07-26 10:32:14 +0300

    add add-card-btn test

---
+++ src/app/add-card-btn/add-card-btn.component.spec.js
@@ -0,0 +1,43 @@
+import { AddCardBtnComponent } from 'app/add-card-btn/add-card-btn.component';
+
+import {
+  beforeEach,
+  beforeEachProviders,
+  describe,
+  expect,
+  it,
+  inject,
+  TestComponentBuilder,
+} from '@angular/core/testing';
+
+describe('BoardComponent test', () => {
+  let tcb;
+
+  beforeEachProviders(() => [
+    TestComponentBuilder,
+    AddCardBtnComponent,
+  ]);
+
+  beforeEach(inject([TestComponentBuilder], _tcb => {
+    tcb = _tcb;
+  }));
+
+  it('emit onAddRow event', (done) => {
+    tcb.createAsync(AddCardBtnComponent).then(fixture => {
+      const component = fixture.componentInstance;
+      const element = fixture.nativeElement;
+      const test = {
+        expectCalled() { },
+      };
+
+      spyOn(test, 'expectCalled');
+      component.onAddCard.subscribe(() => test.expectCalled());
+
+      element.querySelector('.add-card').click();
+
+      expect(test.expectCalled).toHaveBeenCalled();
+
+      done();
+    }).catch(e => done.fail(e));
+  });
+});
