27ba3dabfaf9d20a6564b749c8ed397c4d560fa6
Stas Kogut <staskgt@nixsolutions.com>
2016-07-26 15:16:34 +0300

    add toggle-todo.component test

---
+++ src/app/toggle-todo-btn/toggle-todo-btn.component.spec.js
@@ -0,0 +1,43 @@
+import { ToggleTodoBtnComponent } from 'app/toggle-todo-btn/toggle-todo-btn.component';
+
+import {
+  beforeEach,
+  beforeEachProviders,
+  describe,
+  expect,
+  it,
+  inject,
+  TestComponentBuilder,
+} from '@angular/core/testing';
+
+describe('ToggleTodoBtnComponent test', () => {
+  let tcb;
+
+  beforeEachProviders(() => [
+    TestComponentBuilder,
+    ToggleTodoBtnComponent,
+  ]);
+
+  beforeEach(inject([TestComponentBuilder], _tcb => {
+    tcb = _tcb;
+  }));
+
+  it('should emit onToggle event', (done) => {
+    tcb.createAsync(ToggleTodoBtnComponent).then(fixture => {
+      const component = fixture.componentInstance;
+      const element = fixture.nativeElement;
+      const $checkbox = element.querySelector('.checkbox');
+      const test = { expectCalled() { } };
+
+      spyOn(test, 'expectCalled');
+
+      component.onToggle.subscribe(() => test.expectCalled());
+      $checkbox.click();
+
+      expect(test.expectCalled).toHaveBeenCalled();
+      expect($checkbox.checked).toBeTrue();
+
+      done();
+    }).catch(e => done.fail(e));
+  });
+});
