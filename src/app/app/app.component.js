99ca1270f0bb29e04db71e2d7db2a176819d2fb1
Stas Kogut <staskgt@nixsolutions.com>
2016-07-28 17:43:27 +0300

    add signin route

--- src/app/app/app.component.js
+++ src/app/app/app.component.js
@@ -3,20 +3,29 @@ import { Router, ROUTER_DIRECTIVES } from '@angular/router';
 import { BoardComponent } from 'app/board';
 import { HomeComponent } from 'app/home';
 import { PageNotFoundComponent } from 'app/page-not-found';
+import { LoginFormComponent } from 'app/login-form';
+import { AuthService } from 'service/auth';
 
 @Component({
   selector: 'my-app',
   directives: [ROUTER_DIRECTIVES],
   template: require('./app.component.html'),
   styles: [require('./app.component.css')],
+  providers: [AuthService],
   precompile: [
     BoardComponent,
     HomeComponent,
     PageNotFoundComponent,
+    LoginFormComponent,
   ],
 })
 export class AppComponent {
-  constructor(_router: Router) {
+  constructor(_router: Router, _auth: AuthService) {
     this.router = _router;
+    this.auth = _auth;
+  }
+
+  ngOnInit() {
+    this.auth.isAlreadyLogged();
   }
 }
--- src/app/app/app.routes.js
+++ src/app/app/app.routes.js
@@ -1,11 +1,13 @@
 import { provideRouter, RouterConfig } from '@angular/router';
 import { BoardComponent } from 'app/board';
 import { HomeComponent } from 'app/home';
+import { LoginFormComponent } from 'app/login-form';
 import { PageNotFoundComponent } from 'app/page-not-found';
 
 const routes: RouterConfig = [
   { path: '', component: HomeComponent },
   { path: 'board', component: BoardComponent },
+  { path: 'signin', component: LoginFormComponent },
   { path: '**', component: PageNotFoundComponent },
 ];
 
